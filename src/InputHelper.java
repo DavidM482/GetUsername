8b2bc81628fb78c7a529c41c802934cfc01b8e34
DavidM482 <martin.613901@swl.k12.oh.us>
2024-11-12 08:36:13 -0500

    changed

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,20 +1,140 @@
 import java.util.Scanner;
 
-public class GetUsernameInputHelper {
-    public static void String getZeroLengthString(Scanner scan, String prompt, String zeroLengthString) {
-        String input;
+public class InputHelper {
+
+
+    public static String getNonZeroLenString(Scanner scan, String prompt){
+        String string;
         boolean done = false;
 
         System.out.println(prompt);
         do {
-            input = scan.nextLine();
-            if (input.length() > 0) {
+            string = scan.nextLine();
+            if (string.isEmpty()) {
+                System.out.println("String can't be blank. Please try again.");
+            } else {
+                done = true;
+            }
+        } while (!done);
+        return string;
+    }
+
+
+    public static int getInt(Scanner scan, String prompt){
+        int num = 0; //eventually returned to user after validated
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
                 done = true;
             } else {
-                System.out.println("Sorry, your input is not long enough ");
+                System.out.println("Invalid input. Please try again.");
+                scan.nextLine();
+            }
+        } while (!done);
+        return num;
+    }
+
+    public static double getDouble(Scanner scan, String prompt){
+        double num = 0;
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                num = scan.nextDouble();
+                done = true;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+                scan.nextLine();
+            }
+        } while (!done);
+        return num;
+    }
+
+    public static int getRangedInt(Scanner scan, String prompt, int min, int max){
+        int num = 0;
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
+                if (num >= min && num <= max) {
+                    done = true;
+                } else {
+                    System.out.println("Int has to be within the range. Please try again.");
+                }
+            } else {
+                System.out.println("Invalid input. Please try again.");
             }
             scan.nextLine();
         } while (!done);
-        return input;
+        return num;
+    }
+
+    public static double getRangedDouble(Scanner scan, String prompt, double min, double max){
+        double num = 0;
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                num = scan.nextDouble();
+                if (num >= min && num <= max) {
+                    done = true;
+                } else {
+                    System.out.println("Double has to be within the range. Please try again.");
+                }
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+            scan.nextLine();
+        } while (!done);
+        return num;
+    }
+
+    public static int getPositiveNonZeroInt(Scanner scan, String prompt){
+        int num = 0; //eventually returned to user after validated
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
+                if (num > 0) {
+                    done = true;
+                } else {
+                    System.out.println("Please enter a positive, non-zero integer value.");
+                }
+            } else {
+                System.out.println("Invalid input. Please try again.");
+                scan.nextLine();
+            }
+        } while (!done);
+        return num;
+    }
+
+    public static boolean getYNConfirm(Scanner scan, String prompt){
+        String answer;
+        boolean yn = false;
+        boolean done = false;
+
+        System.out.println(prompt);
+        do {
+            answer = scan.nextLine();
+            if (answer.equalsIgnoreCase("y")) {
+                yn = true;
+                done = true;
+            } else if (answer.equalsIgnoreCase("n")) {
+                yn = false;
+                done = true;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        } while (!done);
+        return yn;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String username = InputHelper.getNonZeroLenString(scan, "Please enter your username:");
+        System.out.println("The username you entered is: " + username);
+    }
 }
